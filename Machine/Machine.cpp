4ff83396fba86965310ede832e201c893bad7feb
Omar61554 <wagih.omar11@gmail.com>
2023-12-09 22:31:21 +0200

    Architrcture

---
+++ Machine/Machine.cpp
@@ -0,0 +1,91 @@
+/************** AUTHOR:             ********************************************************************************/
+/************** DATE: 12/8/2023     ********************************************************************************/
+/************** VERSION: 0.1        ********************************************************************************/
+/************** FILE:  Machine.cpp  ********************************************************************************/
+/*******************************************************************************************************************/
+
+
+
+#include"Machine.hpp"
+
+// constructor
+
+Machine::Machine()
+{
+    /*CODE*/
+}
+
+// destructor
+
+Machine::~Machine()
+{
+    /*CODE*/
+}
+
+// function to handle the user input
+
+void Machine::handleUserInput()
+{
+    /*CODE*/
+}
+
+// function to handle the display products
+
+void Machine::handleDisplayProducts()
+{
+    /*CODE*/
+}
+
+// function to enter program mode
+
+void Machine::enterProgramMode()
+{
+    /*CODE*/
+}
+
+// function to enter operation mode
+
+void Machine::enterOperationMode(Request request)
+{
+    /*CODE*/
+}
+
+// function to add product
+
+void Machine::addProduct()
+{
+    /*CODE*/
+}
+
+// function to edit product
+
+void Machine::editProduct()
+{
+    /*CODE*/
+}
+
+// function to delete product
+
+void Machine::deleteProduct()
+{
+    /*CODE*/
+}
+
+// function to display sales
+
+void Machine::displaySales()
+{
+    /*CODE*/
+}
+
+// function to display requests
+
+void Machine::displayRequests()
+{
+    /*CODE*/
+}
+
+/*******************************************************************************************************************/
+/*******************************************************************************************************************/
+
+
